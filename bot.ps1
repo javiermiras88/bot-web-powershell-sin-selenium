d84e23d379c8a8ca7ec5c1be60ece9313348ba67
javiermiras88 <javiermiras88@hotmail.com>
2018-04-12 21:32:29 +0200

    .

---
+++ bot.ps1
@@ -0,0 +1,18 @@
+﻿cls
+
+$ie = new-object -com "InternetExplorer.Application"
+$ie.navigate2("https://www.youtube.com/?hl=es&gl=ES")
+
+$ie.visible = $true
+
+while($ie.Busy) { Start-Sleep -Milliseconds 100 }
+
+$doc = $ie.Document
+
+$doc.getElementById("masthead-search-term").value = "PowerShell"
+$doc.getElementById("search-btn").click()
+
+
+
+
+
